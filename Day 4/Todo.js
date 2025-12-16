6a96fc5d56338d9f9e0c24ccb0cf4b6e50bf6913
Rushikesh Kharage <rushikeshkharage04@gmail.com>
2025-12-16 12:46:59 +0530

    Todo app

---
+++ Day 4/Todo.js
@@ -0,0 +1,12 @@
+function Send(){
+    let text = document.getElementById("textid");
+    let button = document.getElementById("buttonid");
+
+    let newEle = document.createElement("div")
+    newEle.innerText = 
+    button.addEventListener("click" , 
+
+    )
+    
+
+}
